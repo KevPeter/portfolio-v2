1f3409e504cd7b5020c66c6a1149b79a3ad0985f
kevpeter <kpeter1990@yahoo.com>
2026-02-02 15:13:23 -0800

    Add Navbar with styling

---
+++ src/layout/Navbar.jsx
@@ -0,0 +1,27 @@
+const navLinks = [
+    {href: "#about", label: "About"},
+    {href: "#experience", label: "Experience"},
+    {href: "#projects", label: "Projects"},
+];
+
+export const Navbar = () => {
+    return (
+    <header className="fixed top-0 left-0 right-0 py-5">
+        <nav className="container mx-auto px-6 flex items-center justify-between">
+            <a href="#" className="text-xl font-bold tracking-tight">
+                KP<span>.</span>
+            </a>
+
+            {/* Desktop Nav */}
+            <div className="flex items-center gap-1">
+                <div className=" px-2 py-1 flex items-center gap-1">
+                    {navLinks.map((link, index) => (
+                        <a href={link.href} key={index} className="px-4 no-underline hover:underline">
+                            {link.label}
+                        </a>
+                    ))}
+                </div>
+            </div>
+        </nav>
+    </header>);
+}
